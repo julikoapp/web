dbd5c1a7227fe9a4ea69cd6dfd034ab16e1cff59
Julia <yula.barko@gmail.com>
2020-12-20 07:29:01 +0000

    Создание Django приложения

--- init.sh
+++ init.sh
@@ -7,4 +7,11 @@ sudo /etc/init.d/gunicorn restart
 
 cd /home/box/web/
 
-sudo gunicorn -c /home/box/web/hello.py hello:wsgi_application
+#sudo gunicorn -c /home/box/web/hello.py hello:wsgi_application
+#sudo nano /usr/sbin/gunicorn-debian
+sudo nano /usr/bin/gunicorn
+sudo nano /usr/bin/gunicorn_django
+sudo nano /usr/bin/gunicorn_paster
+sudo ln -sf /home/box/web/etc/django_conf.py /etc/gunicorn.d/django_conf.py
+
+sudo gunicorn -c /etc/gunicorn.d/django_conf.py ask.wsgi:application
