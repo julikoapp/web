666ebec194c732e3153e9e1efba66213a9a239cd
julikoapp <59240959+julikoapp@users.noreply.github.com>
2020-12-20 11:01:13 +0600

    Update init.sh
    bash init.sh - и все заработало

--- init.sh
+++ init.sh
@@ -1,8 +1,10 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/hello.py
 
-
+sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/test
 sudo /etc/init.d/gunicorn restart
 
-gunicorn --bind='0.0.0.0:8080' hello:wsgi_application
+cd /home/box/web/
+
+sudo gunicorn -c /home/box/web/hello.py hello:wsgi_application
