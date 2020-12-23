038b4c3538cd5719e0bff5ddbb38a27d9d6d4564
julikoapp <59240959+julikoapp@users.noreply.github.com>
2020-12-23 08:08:49 +0600

    Создание моделей в Django приложении

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -4,6 +4,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class QuestionManager(models.Manager):
+	def new(self):
+		return self.order_by('-added_at')
+	def popular(self):
+		return self.order_by('-rating')
+	
 class Question(models.Model):
 	title = models.CharField(max_length=255)
 	text = models.TextField()
@@ -11,6 +18,7 @@ class Question(models.Model):
 	rating =models.IntegerField(default=0)
 	author = models.ForeignKey(User,on_delete=models.CASCADE)
 	likes = models.ManyToManyField(User, related_name='question_like_user')
+	objects = QuestionManager() 
 
 class Answer(models.Model):
 	text =  models.TextField()
@@ -18,9 +26,4 @@ class Answer(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
 
-class QuestionManager(models.Manager):
-	def new(self):
-		return self.order_by('-added_at')
-	def popular(self):
-		return self.order_by('-rating')
 
